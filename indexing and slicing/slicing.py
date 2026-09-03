e54d29fcd15d12c6af66dc4eb390a47d78df0c17
Kavya Negi <negikavya09@gmail.com>
2026-09-04 00:51:53 +0530

    Changes in slicing

--- indexing and slicing/slicing.py
+++ indexing and slicing/slicing.py
@@ -9,12 +9,12 @@ print(b)
 print(c)
 
 #1d array slicing
-
+print("1D element")
 print(a[2:5])
 print(a[2:5:2] )#jump with 2 , give alternate number
 
 #2d array slicing
-
+print("2D element")
 print(b[0,:]) # 0 index row ke sare element(:)
 print(b[:,2] ) #2nd column ke sare element
 print(b[1:,1:3])#5 6,9 10 aayega mtlb 1 ke baad vale sare row or 1 or 2 column
@@ -25,7 +25,7 @@ print(b[0:2,1:])
 
 
 #3d array slicing
-
+print("3D element")
 print(c[1])#2nd 3d array
 print(c[::2])#1st and last 3d array
 print(c[0,1:])#1st 3d array 2nd row all the columns
